3a5ddee33490fe15b53d3946abc37773909a11cd
Oksana Vasyliuk <streranoksana@gmail.com>
2024-03-11 16:00:45 +0200

    hw2

---
+++ main.py
@@ -0,0 +1,67 @@
+num_1 = 90
+print(type(num_1))
+
+num_2 = 3,33
+print(type(num_2))
+
+string = "olala"
+print(type(string))
+
+name = True
+print(type(name))
+
+lst = ['my', 'ok', 'name', 'Tom']
+print(type(lst))
+
+typle = (1, 1 , 1, 1)
+print(type(typle))
+
+dictionary = {"name": "May", "age": 45}
+print(type(dictionary))
+
+set_y = {1,2, 3,}
+print(type(set_y))
+
+print(type(None))
+
+
+num_str = 125
+print(type(num_str))
+num_1 = str(num_str)
+print(type(num_1))
+
+message = 'Hi, my name is Python!'
+print(message.replace( 'y', '0').replace('i', '1'))
+
+split_test = 'This is a split test'
+str = split_test.split(  )
+print(split_test)
+
+split_test = 'This is a split test'
+split_test = " ".join(split_test)
+print(split_test)
+print(len(split_test))
+
+
+
+list_append = [1, 2, 3]
+print(len(list_append))
+list_append.append(4)
+print(list_append)
+list_append.append(5)
+print(list_append)
+
+list_extend = [4, 5, 6]
+print(list_extend)
+list_extend.extend([7, 8, 9])
+print(list_extend)
+
+dict_test = {'car': 'Toyota', 'price': 4900, 'where': 'EU'}
+print(dict_test.keys())
+print(dict_test.values())
+print(dict_test.items())
+print(dict_test['car'])
+print(dict_test['where'])
+
+print(list_extend.index(6))
+
